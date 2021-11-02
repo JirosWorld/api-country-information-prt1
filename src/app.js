e14d1992a0e08641c5099a53f51ef2a85791840d
jolarti <jolarti@gmail.com>
2021-11-02 17:01:55 +0100

    install Axios and add module src JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
 console.log('Hallo daar!');
+
+// [] houd hier notities en pseudocode bij
+// [] importeer object via norrmale api
+// en dan kun je zometeen proberen om de naam van het allereerste land (eerste in de array) weer te geven op de pagina!
